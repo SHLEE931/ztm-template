344b3f6fff34d7160d4a24e6c45ef8e64b86aa37
SHLEE931 <lshock211093@gmail.com>
2022-03-29 13:50:12 +0800

    Updated homepage with title and slogan.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl" style = "font-size:100px">Welcome to Inventory Management</h1>
+      <p class="mt-5">A platform that gives you complete control of your entire inventory.</p>
+      <a href="https://www.w3schools.com/">Product</a>
+      <img src="Warehouse.jpg" alt="Warehouse" style="width:1700px;height:600px;">
     </div>
   </div>
 </template>
