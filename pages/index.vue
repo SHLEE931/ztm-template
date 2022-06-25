edefa93cfca17ee242eb868d6bd5cc9c1d7ba291
SHLEE <lshock211093@gmail.com>
2022-06-26 01:32:13 +0800

    Done Front Page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <div
-    class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to</h1>
-      <p class="mt-5">Turn your ideas into working web application.</p>
+  <div id='header' class="bg-[url('/DeepSkyBlue.jpg')] px-20">
+    <div id='menu' class='grid grid-cols-2'>
+      <div id='menu-left'>
+        <img src="QorvoLogo4.png" class="w-[200px]" alt=""/>
+      </div>
+      <div id='menu-right'>
+        <ul class="grid grid-cols-4 text-3xl font-oswald text-white uppercase pt-10">
+          <li>Hardware</li>
+          <li>Instrument</li>
+          <li>Components</li>
+          <li>Check Out</li>
+        </ul>
+      </div>
+    </div>
+    <div class="grid grid-cols-2">
+      <img src="Hardware.jpg" class="pr-8 pb-5 mt-10" alt="" />
+      <div class="py-20">
+        <h1 class="text-8xl text-white text-center font-lilita uppercase">Inventory</h1>
+        <h2 class="text-6xl text-white text-center font-lilita uppercase my-5">Management System</h2>
+        <p class="text-white font-roboto font-light text-center">
+          Keep track on all the instrument, hardware and components available at Malaysia Qorvo.
+        </p>
+      </div>
     </div>
   </div>
 </template>
