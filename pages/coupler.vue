d8fc7ff3bf6dc957f585a8709f3360cbe1fe3b01
SHLEE <lshock211093@gmail.com>
2022-07-11 01:04:11 +0800

    Implemented database funciton

---
+++ pages/coupler.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div id="header" class="bg-[url('/DeepSkyBlue.jpg')] px-20">
+      <div id="menu" class="grid grid-cols-2">
+        <div id="menu-left">
+          <NuxtLink to="/"><img src="/QorvoLogo4.png" class="w-[200px]" alt="" /></NuxtLink>
+        </div>
+        <div id="menu-right">
+          <ul class="md:grid grid-cols-4 text-lg font-oswald uppercase pt-10">
+            <li class="text-white">Hardware</li>
+            <li class="text-white">Instrument</li>
+            <NuxtLink to="/menu" class="text-white">Components</NuxtLink>
+            <NuxtLink to="/cart" class="text-white">Cart ({{ $store.getters.totalItems }})</NuxtLink>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div class="py-20">
+          <h1
+            class="text-6xl text-white text-center font-oswald uppercase pt-24 pb-28"
+          >
+            Coupler
+          </h1>
+        </div>
+      </div>
+    </div>
+    <div id="items" class="md:grid grid-cols-10 gap-x-5 gap-y-16 mt-10 mx-50 lg:grid grid-cols-10 gap-x-5 gap-y-16 mt-10 mx-50 xl:grid grid-cols-10 gap-x-5 gap-y-16 mt-10 mx-50">
+      <MenuItem1 v-for="b in items" :key="b.name" :item="b" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MenuItem1 from '~/components/MenuItem1.vue';
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  mounted() {
+    this.$axios.get('items.json').then((response) => {
+      this.items = response.data.coupler;
+    });
+  },
+  components: { MenuItem1 },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,26 @@
         <NuxtLink to="/"><img src="/QorvoLogo4.png" class="w-[200px]" alt="" /></NuxtLink>
       </div>
       <div id="menu-right">
-          <ul class="md:grid grid-cols-4 text-lg font-oswald uppercase pt-10">
-            <li class="text-white">Hardware</li>
-            <li class="text-white">Instrument</li>
-            <NuxtLink to="/menu" class="text-white">Components</NuxtLink>
-            <NuxtLink to="/cart" class="text-white">Cart ({{ $store.getters.totalItems }})</NuxtLink>
+          <ul class="md:grid grid-cols-2 text-lg font-oswald uppercase pt-10">
+            <button @click="isOpen = !isOpen" class="text-white">Menu</button>
+            <!--<button class="px-4 py-2 text-white">Cart</button>-->
+            <button class="px-4 py-2 text-white"><NuxtLink to="/cart" class="text-white">Cart ({{ $store.getters.totalItems }})</NuxtLink></button>
+            <!--<NuxtLink to="/cart" class="text-white">Cart ({{ $store.getters.totalItems }})</NuxtLink>-->
+            <div v-if="isOpen" class="md:grid grid-row-2 text-lg font-oswald uppercase text-center bg-white rounded-lg ">
+              <a href="#" class="block px-4 py-2 hover:bg-gray-500 hover:text-white">Component</a>
+              <a href="#" class="block px-4 py-2 hover:bg-gray-500 hover:text-white">Hardware</a>
+            </div>
+            <!--<ul v-if="isOpen" class="md:grid grid-row-2 text-lg font-oswald uppercase text-center bg-white rounded-lg ">
+              <il><a href="#" class="block px-4 py-2 hover:bg-gray-500 hover:text-white">Component</a></il>
+              <il><a href="#" class="block px-4 py-2 hover:bg-gray-500 hover:text-white">Hardware</a></il>
+            </ul>-->
+            <!--<button class="text-white">Cart</button>-->
+            <!--<div v-if="isOpen" class="bg-white rounded-lg py-2 items-enter">-->
+            <!--</div>-->
+            <!--<li class="text-white">Hardware</li>-->
+            <!--<li class="text-white">Instrument</li>-->
+            <!--<NuxtLink to="/menu" class="text-white">Components</NuxtLink>-->
+            <!--<NuxtLink to="/cart" class="text-white">Cart ({{ $store.getters.totalItems }})</NuxtLink>-->
           </ul>
       </div>
     </div>
@@ -30,5 +45,85 @@
       </div>
     </div>
   </div>
+  <div id="Menu">
+    <h2 class="text-center text-blue-400 md:text-3xl lg:text-5xl xl:text-7xl">Menu</h2>
+  </div>
+  <table class="table-auto w-2/3 mt-20 mx-auto border border-black-500 mb-10">
+  <thead>
+    <tr>
+      <th class="text-left">Product</th>
+      <th class="text-left">Component</th>
+      <th class="text-left">Hardware</th>
+      <th class="text-left">LoadBoard</th>
+    </tr>
+  </thead>
+  <tbody class="border border-black-500">
+    <tr class="border border-black-500">
+      <td><NuxtLink to="/filter">Filter</NuxtLink></td>
+      <td>Capacitor</td>
+      <td>Dsub 20</td>
+    </tr>
+    <tr class="border border-black-500">
+      <td><NuxtLink to="/attenuator">Attenuator</NuxtLink></td>
+      <td>Inductor</td>
+      <td>VHDCI</td>
+    </tr>
+    <tr class="border border-black-500">
+      <td><NuxtLink to="/coupler">Coupler</NuxtLink></td>
+      <td>Resistor</td>
+      <td>Dsub 60</td>
+    </tr>
+    <tr class="border border-black-500">
+      <td><NuxtLink to="/thru">Thru</NuxtLink></td>
+      <td>Capacitor</td>
+      <td>N/A</td>
+    </tr>
+    <tr class="border border-black-500">
+      <td><NuxtLink to="/Terminator">Terminator</NuxtLink></td>
+      <td>N/A</td>
+      <td>N/A</td>
+    </tr>
+    <tr class="border border-black-500">
+      <td><NuxtLink to="/adapter">Adapter</NuxtLink></td>
+      <td>N/A</td>
+      <td>N/A</td>
+    </tr>
+    <tr class="border border-black-500">
+      <td><NuxtLink to="/biastee">Bias Tee</NuxtLink></td>
+      <td>N/A</td>
+      <td>N/A</td>
+    </tr>
+    <tr class="border border-black-500">
+      <td><NuxtLink to="/dcblock">DC Block</NuxtLink></td>
+      <td>N/A</td>
+      <td>N/A</td>
+    </tr>
+    <tr class="border border-black-500">
+      <td><NuxtLink to="/limiter">Limiter</NuxtLink></td>
+      <td>N/A</td>
+      <td>N/A</td>
+    </tr>
+    <tr class="border border-black-500">
+      <td>Impedance matching pad</td>
+      <td>N/A</td>
+      <td>N/A</td>
+    </tr>
+    <tr class="border border-black-500">
+      <td><NuxtLink to="/rfchokes">RF Chokes</NuxtLink></td>
+      <td>N/A</td>
+      <td>N/A</td>
+    </tr>
+  </tbody>
+</table>
 </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
